652b7654a6177aa510a198e2fa66f0c37ad5ad88
jollygrin <jollygrin@protonmail.com>
2020-04-04 21:57:38 +0200

    update

--- pages/online/[lobby]/[player]/index.js
+++ pages/online/[lobby]/[player]/index.js
@@ -2,13 +2,15 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 // import Header from '../../../components/header'
 
-const Lobby = () => {
+const Player = () => {
   const router = useRouter()
-  const { lobby } = router.query
+  const { lobby, player } = router.query
 
   return (
     <>
-      <h1>Lobby: {lobby}</h1>
+      <h1>
+        Lobby: {lobby} - Player: {player}
+      </h1>
       <span>
         <a href='#'>Spectate</a> or <a href='#'>Load Deck</a>
       </span>
@@ -17,4 +19,4 @@ const Lobby = () => {
   )
 }
 
-export default Lobby
+export default Player
