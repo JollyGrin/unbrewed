d56f6dcabc1842bf698b16512b3c608387c42144
jollygrin <jollygrin@protonmail.com>
2020-04-05 17:17:09 +0200

    adding routing to [lobby] class component

--- pages/online/[lobby]/index.js
+++ pages/online/[lobby]/index.js
@@ -1,50 +1,26 @@
 import React, { Component } from 'react'
-import { useRouter } from 'next/router'
+import { withRouter } from 'next/router'
 import Link from 'next/link'
 import Lobby from '../../../components/online/Lobby'
 import Test from '../../../components/online/Test'
-// import Header from '../../../components/header'
 
-// const Lobby = () => {
-//   const router = useRouter()
-//   const { lobby } = router.query
-
-//   const playerInput = React.createRef()
-
-//   return (
-//     <>
-//       <h1>Lobby: {lobby}</h1>
-//       <span>
-//         <a href='#'>Spectate</a> or{' '}
-//         <a href={`/online/${lobby}/${playerInput.current.value}`}>Load Deck</a>
-//         <input ref={playerInput}></input>
-//       </span>
-//       <hr />
-//     </>
-//   )
-// }
-
-// export default Lobby
-
-// const TestWithRouter = props => {
-//   const router = useRouter()
-//   return <Test {...props} router={router} />
-// }
-
-export default class indexLobby extends Component {
+class indexLobby extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      playerInputBox: 'player1'
-    }
-  }
 
-  changeInputBox = e => {
-    console.log(e)
+    this.state = {}
   }
 
   render () {
-    // const router = useRouter()
-    return <div>Lobby</div>
+    const { router } = this.props
+    console.log('router', router)
+    return (
+      <div>
+        <p>Lobby</p>
+        <p>{JSON.stringify(router.query)}</p>
+      </div>
+    )
   }
 }
+
+export default withRouter(indexLobby)
